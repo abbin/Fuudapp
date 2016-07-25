9982ce491ff4d4f0ab3d0ccf904632f682bee845
abbin <abn@fuudapp.com>
2016-07-25 20:37:00 +0530

    Comm

--- Fuudapp/AppDelegate.m
+++ Fuudapp/AppDelegate.m
@@ -16,7 +16,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    [[UINavigationBar appearance] setBarTintColor:[UIColor whiteColor]];
+    [[UITabBar appearance] setTintColor:[UIColor redColor]];
     return YES;
 }
 
