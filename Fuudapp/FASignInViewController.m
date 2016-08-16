7f43e77ab4d6c576f82327cdc477dc7374fdc8d8
abbin <abn@fuudapp.com>
2016-08-16 21:33:20 +0530

    Comm

---
+++ Fuudapp/FASignInViewController.m
@@ -0,0 +1,37 @@
+//
+//  FASignInViewController.m
+//  Fuudapp
+//
+//  Created by Abbin Varghese on 16/08/16.
+//  Copyright © 2016 Fuudapp. All rights reserved.
+//
+
+#import "FASignInViewController.h"
+
+@interface FASignInViewController ()
+
+@end
+
+@implementation FASignInViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
--- Fuudapp/FATabBarController.m
+++ Fuudapp/FATabBarController.m
@@ -28,24 +28,9 @@
 
 -(void)viewDidAppear:(BOOL)animated{
     [super viewDidAppear:animated];
-    [[FIRAuth auth] addAuthStateDidChangeListener:^(FIRAuth *_Nonnull auth,
-                                                    FIRUser *_Nullable user) {
-        if (user != nil) {
-            // User is signed in.
-        } else {
-            // No user is signed in.
-        }
-    }];
+    if ([FIRAuth auth].currentUser == nil) {
+        [self performSegueWithIdentifier:@"FASignInViewControllerSegue" sender:self];
+    }
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
